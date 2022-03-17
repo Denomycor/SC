900edd56138f9cc3ca9a9cc667e6b03592ef907c
Denomycor <afonsoresteves@hotmail.com>
2022-03-17 10:01:39 +0000

    Changed connection classes

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -1,10 +1,17 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 
-public class ClientConnection extends Connection {
+public class ClientConnection {
 
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    
     public void openSocket(String address) throws UnknownHostException, IOException{
         Integer port = 45678;
         
@@ -15,6 +22,36 @@ public class ClientConnection extends Connection {
         }
 
         socket = new Socket(address, port);
+    }
+
+    public void closeSocket() throws IOException{
+        socket.close();
+    }
+
+    public void openConnection() throws IOException{ 
+        in = new ObjectInputStream(socket.getInputStream());
+        out = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public void closeConnection() throws IOException{
+        in.close();
+        out.close();
+    }
+    
+    public String read() throws ClassNotFoundException, IOException{
+        return (String) in.readObject();
+    }
+    
+    public void write(String write) throws IOException{
+        out.writeObject(write);
+    }
+    
+        public Socket getSocket(){
+            return socket;
+        }
+
+    public void setSocket(Socket socket){
+        this.socket = socket;
     }
 
 }
--- src/ServerConnection.java
+++ src/ServerConnection.java
@@ -2,15 +2,17 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 
-public class ServerConnection extends Connection{
+public class ServerConnection{
     private ServerSocket svSocket;
 
     public void openListeningSocket(Integer port) throws IOException{
         svSocket = new ServerSocket(port);
     }
 
-    public void listen() throws IOException{
-        socket = svSocket.accept();
+    public ClientConnection listen() throws IOException{
+        ClientConnection connection = new ClientConnection();
+        connection.setSocket(svSocket.accept());
+        return connection;
     }
 
     public void closeListeningSocket() throws IOException{
