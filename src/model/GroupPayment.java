6ce654794049ea761fe05cfe0461fbc8d0cff786
JanekaPoleca <joaoaganjos@gmail.com>
2022-04-30 21:45:02 +0100

    Load state and commit state

--- src/model/GroupPayment.java
+++ src/model/GroupPayment.java
@@ -2,40 +2,52 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GroupPayment {
 	
-	private Group group;
-	private List<PaymentRequest> active;
-	private List<PaymentRequest> complete;
+	private String groupPayId;
+	private List<PaymentRequest> payments;
+	private String groupId;
 	
-	public GroupPayment(Group group) {
-		this.group = group;
-		this.active = new ArrayList<>();
-		this.complete = new ArrayList<>();
+	public GroupPayment(String id, String groupId) {
+		this.groupPayId = id;
+		this.groupId = groupId;
+		this.payments = new ArrayList<>();
 	}
 	
 	public boolean isPaid() {
-		return active.isEmpty();
+		return payments.stream()
+				.filter(p -> p.isPaid())
+				.collect(Collectors.toList()).size() == payments.size();
 	}
 	
 	public void addPayment(PaymentRequest payment) {
-		active.add(payment);
+		payments.add(payment);
 	}
 	
 	public List<PaymentRequest> getActive() {
-		return this.active;
+		return payments.stream()
+				.filter(p -> !p.isPaid())
+				.collect(Collectors.toList());
 	}
 	
 	public List<PaymentRequest> getComplete() {
-		return this.complete;
+		return payments.stream()
+				.filter(p -> p.isPaid())
+				.collect(Collectors.toList());
+	}
+	
+	public List<PaymentRequest> getPayments() {
+		return payments;
 	}
 
-	public void updatePayment(PaymentRequest payment) {
-		active.remove(payment);
-		complete.add(payment);
-		if(active.isEmpty()) {
-			group.updatePayment(this);
-		}
+	public String getId() {
+		return groupPayId;
 	}
+	
+	public String getGroupId() {
+		return groupId;
+	}
+	
 }
