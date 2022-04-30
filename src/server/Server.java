5a255990b7dec818c125281c043b1ba4fcf2936c
Vicente <fc55386@alunos.fcul.ul.pt>
2022-04-30 19:18:42 +0100

    Removed unused import

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
