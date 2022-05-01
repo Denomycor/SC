d3b88704addfbbf50e1871f02cf093c48bfcca45
Vicente <fc55386@alunos.fcul.ul.pt>
2022-05-01 16:23:50 +0100

    .gitignore updated and compile updated

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 echo Compiling...
 javac -sourcepath src src/*/*.java -d bin
 cd bin
-jar cfm Trokos.jar ../manifestClient.mf client/*.class exceptions/*.class network/*.class
+jar cfm Trokos.jar ../manifestClient.mf client/*.class exceptions/*.class network/*.class model/*.class 
 jar cfm TrokosServer.jar ../manifestServer.mf model/*.class exceptions/*.class network/*.class server/*.class
 mv Trokos.jar ..
 mv TrokosServer.jar ..
